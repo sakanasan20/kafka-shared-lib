9d2c258a6e6744b974c46b96c0b67bd1cdae987a
Nick Chen <nick.chen@cmp.com.tw>
2025-11-13 09:52:00 +0800

    Initial commit

---
+++ src/main/java/com/niqdev/kafka/KafkaSharedLibApplication.java
@@ -0,0 +1,13 @@
+package com.niqdev.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSharedLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSharedLibApplication.class, args);
+	}
+
+}
