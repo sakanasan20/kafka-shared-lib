3a722018b7c88b92b9993fa8985a850fac5cefb7
Nick Chen <nick.chen@cmp.com.tw>
2025-11-18 09:27:02 +0800

    feat: add event and error

---
+++ src/main/java/com/niqdev/kafka/shared/error/NotRetryableException.java
@@ -0,0 +1,11 @@
+package com.niqdev.kafka.shared.error;
+
+public class NotRetryableException extends RuntimeException {
+    public NotRetryableException(Exception exception) {
+        super(exception);
+    }
+
+    public NotRetryableException(String message) {
+        super(message);
+    }
+}
